5f191e4e432ec83ecba571098b170c93750773fc
Stefan Stroe <stroestefan97@gmail.com>
2020-12-04 10:42:48 +0200

    Add script to open most used programs on startup

---
+++ startup/logStartTime.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Log the daily start time
+date '+%H:%M %a, %d %b %Y' >> ~/misc/program/program.txt
---
+++ startup/startUpPrograms.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Log the daily start time
+./misc/program/logStartTime.sh;
+
+# Open the terminal
+gnome-terminal;
+
+# Open Sublime
+subl;
+
+# opens the browser
+google-chrome&;
+
+# open Intellij
+~/IdeaProjects/idea;
