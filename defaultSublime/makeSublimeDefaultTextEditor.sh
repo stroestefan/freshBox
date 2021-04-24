4d493de71ea759ffce65a7834f0e0c40f1695c5b
stroestefan <stroestefan97@gmail.com>
2021-04-24 10:01:38 +0200

    Add sublime to one more default place

--- defaultSublime/makeSublimeDefaultTextEditor.sh
+++ defaultSublime/makeSublimeDefaultTextEditor.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sed -i 's/gedit/sublime_text/g' /usr/share/applications/defaults.list
+sed -i 's/ubuntusdk.desktop/org.gnome.sublime_text.desktop/g' /usr/share/applications/defaults.list
